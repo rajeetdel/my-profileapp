34f781ead61e9706dd970b8490ce28e4ddfe77a7
Rajeet <rajeetdel@gmail.com>
2019-07-25 19:09:17 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Circle extends React.Component {
+  render() {
+    return (
+      <button className="circleA" onClick={function() { alert('click'); }}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Telephone extends React.Component {
+  render() {
+    return (
+      <div className="circleOuter">
+      <Circle />
+        <div className="circleInner">          
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <Telephone />,
+  document.getElementById('root')
+);
