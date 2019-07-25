47f50060e15a60713c8e674bd49e244faf437045
Rajeet <rajeetdel@gmail.com>
2019-07-25 21:51:55 +0530

    Updates

---
+++ precache-manifest.550f889257debb93f3fb34d290c7ed49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7c823d65f983cda530e2c971a8d08d",
+    "url": "/my-profileapp/index.html"
+  },
+  {
+    "revision": "328bae40309571ab859a",
+    "url": "/my-profileapp/static/css/main.4697979b.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/my-profileapp/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "328bae40309571ab859a",
+    "url": "/my-profileapp/static/js/main.2b0e32e2.chunk.js"
+  },
+  {
+    "revision": "ff491ce469d037d60bbc",
+    "url": "/my-profileapp/static/js/runtime~main.63aa8a1e.js"
+  }
+]);
