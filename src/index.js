7d54bf34fce1b79ba1e6ff17a4276334ae1033c8
Rajeet <rajeetdel@gmail.com>
2019-07-25 20:47:22 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Circle extends React.Component {
-  render() {
-    return (
-      <button className="circleA" onClick={function() { alert('click'); }}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
 class Telephone extends React.Component {
+    
+    constructor(props) {
+    super(props);
+    this.state = {
+      arr_circleA: [],
+      // 0 = not started, 1 = in progress, 2= finished
+      status: 0,
+      // using keycodes to indicate direction
+      direction: 39
+    };
+    
+    this.setDirection = this.setDirection.bind(this);
+    this.moveCircleA = this.moveCircleA.bind(this);
+    this.startDial = this.startDial.bind(this);
+    this.endDial = this.endDial.bind(this);    
+  }
+    
   render() {
     return (
       <div className="circleOuter">
@@ -25,8 +31,15 @@ class Telephone extends React.Component {
   }
 }
 
-
-
+class Circle extends React.Component {
+  render() {
+    return (
+      <button className="circleA" onClick={function() { alert('click'); }}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
 // ========================================
 
