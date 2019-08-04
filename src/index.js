432fbc47881a0177ec5ab382c8cbfb417f26d836
Rajeet <rajeetdel@gmail.com>
2019-08-05 00:13:13 +0530

    Moving A

--- src/index.js
+++ src/index.js
@@ -1,44 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Telephone extends React.Component {
     
     constructor(props) {
     super(props);
     this.state = {
-      arr_circleA: [],
-      // 0 = not started, 1 = in progress, 2= finished
-      status: 0,
-      // using keycodes to indicate direction
-      direction: 39
+      classes: "circleAA"
     };
     
-    this.moveCircleA = this.moveCircleA.bind(this);
-    this.startDial = this.startDial.bind(this); 
+    this.startDialA = this.startDialA.bind(this); 
   }
   
-    // move circle A
-  moveCircleA() {
-    this.setState({ arr_circleA: [0, 0] });
-  }
-  
-  startDial() {
-    this.moveCircleA();
-    this.setState({
-      status: 1,
-      arr_circleA: [1, 1]
-    });
+
+  startDialA() {
+    this.setState({classes: "circleA"});
   }
     
   render() {
+
     return (
-      <div className="circleOuter">
-        <button className="circleA" onClick={this.startDial}>
-        <p>A</p>
-        </button>  
-        <div className="circleInner"></div>
-      </div>
+      <div className="circleOuter">  
+      <button onClick= {this.startDialA} className={this.state.classes}><p>A</p></button>
+      <div className="circleInner"></div>    
+      </div>  
     );
   }
 }
